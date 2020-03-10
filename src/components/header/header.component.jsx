43aeac078667b95c4d69e11fc0c9b6bf4df2f30a
BrunoSantanaBR <bruno.raphael.marques.santana@gmail.com>
2020-03-10 21:19:20 +0100

    Created cart item component and connected our cart dropdown with the cartItems reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,7 +6,7 @@ import { auth } from "../../firebase/firebase.utils";
 
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import CartIcon from "../cart-icon/cart-icon.component";
-import Cart from "../cart-dropdown/cart-dropdown.component";
+import CartDropDown from "../cart-dropdown/cart-dropdown.component";
 
 import "./header.styles.scss";
 
@@ -33,7 +33,7 @@ const Header = ({ currentUser, hidden }) => (
       )}
       <CartIcon />
     </div>
-    {hidden ? null : <Cart />}
+    {hidden ? null : <CartDropDown />}
   </div>
 );
 
